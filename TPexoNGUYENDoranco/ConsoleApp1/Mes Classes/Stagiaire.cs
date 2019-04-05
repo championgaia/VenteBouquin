0b0bbe06fb88e0e49a104c4bb12ed219412575d3
championgaia <championgaia@gmail.com>
2019-04-05 16:08:13 +0200

    final commit 05042019

---
+++ TPexoNGUYENDoranco/ConsoleApp1/Mes Classes/Stagiaire.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Mes_Classes
+{
+    public class Stagiaire : Personne
+    {
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public string Matiere { get; set; }
+        public Stagiaire()
+        {
+
+        }
+        public Stagiaire(string nom, string prenom, string adresse, bool emploi, DateTime dateDebut,
+            DateTime dateFin, string matiere) : base (nom, prenom, adresse, emploi)
+        {
+            DateDebut = dateDebut; DateFin = dateFin; Matiere = matiere;
+        }
+        public TimeSpan DureeStage()
+        {
+            return DateFin - DateDebut;
+        }
+        public void Etudier(TimeSpan duree, string nomMatiere)
+        {
+            Console.WriteLine("fait le matière " + Matiere
+                + " pendant " + DureeStage().TotalDays);
+        }
+        public void Afficher()
+        {
+            Console.WriteLine("Stagiaire " + Nom + " " + Prenom);
+            Etudier(DureeStage(), Matiere);
+        }
+    }
+}
---
+++ tiktaktotProject/tiktaktot01_01/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tiktaktot01_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button  cliqué", "Message");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 10 cliqué", "Message");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 1 cliqué", "Message");
+            button1.Text = "Off";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 2 cliqué", "Message");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 3 cliqué", "Message");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 4 cliqué", "Message");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 5 cliqué", "Message");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 6 cliqué", "Message");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 7 cliqué", "Message");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 8 cliqué", "Message");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 9 cliqué", "Message");
+        }
+
+    }
+}
