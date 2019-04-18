4b17fe14adbf4517c27d447adffe34da010830f9
Championgaia <championgaia@gamil.com>
2019-04-18 20:55:43 +0200

    modify requete linq

--- VenteBouquinVHN/VenteBouquin_DATA/Class DATA/LivreData.cs
+++ VenteBouquinVHN/VenteBouquin_DATA/Class DATA/LivreData.cs
@@ -40,37 +40,6 @@ namespace VenteBouquin_DATA.Class_DATA
                 .ToList();
             foreach (var item in listeLivreCodeISBN)
             {
-                ListeLivre.Add(new LivreData {
-                    CodeISBN = item.CodeISBN,
-                    NomLivre = item.NomLivre,
-                    Auteur = item.Auteur,
-                    Editeur = item.Editeur,
-                    CoverImage = item.CoverImage,
-                    Prix = (double)item.Prix,
-                    Description = new DescriptionData {
-                        CodeDescription = item.Description.IdDescription,
-                        CodeISBN = item.Description.CodeISBN,
-                        Detail =  item.Description.Detail },
-                    LaCategory = new LivreCategoryDatas(item.FkLivreCategory).ListeCategory.FirstOrDefault()
-                });
-            }
-
-        }
-        #endregion
-        #region Constructeur par codeCategory
-        public LivreDatas(int codeCategory)
-        {
-            ListeLivre = new List<LivreData>();
-            //besoin contexte
-            var listeLivreAvecCodeCategory = context.Livres
-                .Include("LivreCategory")
-                .Include("Description")
-                .Select(c => c)
-                .Distinct()
-                .Where(c => c.FkLivreCategory == codeCategory)
-                .ToList();
-            foreach (var item in listeLivreAvecCodeCategory)
-            {
                 ListeLivre.Add(new LivreData
                 {
                     CodeISBN = item.CodeISBN,
@@ -88,6 +57,99 @@ namespace VenteBouquin_DATA.Class_DATA
                     LaCategory = new LivreCategoryDatas(item.FkLivreCategory).ListeCategory.FirstOrDefault()
                 });
             }
+            //version 2
+            var liste2 = context.Livres
+                .Where(c => c.CodeISBN == codeISBN)
+                .ToList();
+            foreach (var livre in liste2)
+            {
+                ListeLivre.Add(new LivreData
+                {
+                    CodeISBN = livre.CodeISBN,
+                    NomLivre = livre.NomLivre,
+                    Auteur = livre.Auteur,
+                    Editeur = livre.Editeur,
+                    CoverImage = livre.CoverImage,
+                    Prix = (double)livre.Prix,
+                    Description = new DescriptionData
+                    {
+                        CodeDescription = livre.Description.IdDescription,
+                        CodeISBN = livre.Description.CodeISBN,
+                        Detail = livre.Description.Detail
+                    },
+                    LaCategory = new LivreCategoryData
+                    {
+                        CodeCategory = livre.LivreCategory.IdCategory,
+                        NomCategory = livre.LivreCategory.NomCategory
+                    }
+                });
+
+            }
+        }
+        #endregion
+        #region Constructeur par codeCategory
+        public LivreDatas(int codeCategory)
+        {
+            ListeLivre = new List<LivreData>();
+            //besoin contexte
+            #region Version 1
+            //var listeLivreAvecCodeCategory = context.Livres
+            //    .Include("LivreCategory")
+            //    .Include("Description")
+            //    .Select(c => c)
+            //    .Distinct()
+            //    .Where(c => c.FkLivreCategory == codeCategory)
+            //    .ToList();
+            //foreach (var item in listeLivreAvecCodeCategory)
+            //{
+            //    ListeLivre.Add(new LivreData
+            //    {
+            //        CodeISBN = item.CodeISBN,
+            //        NomLivre = item.NomLivre,
+            //        Auteur = item.Auteur,
+            //        Editeur = item.Editeur,
+            //        CoverImage = item.CoverImage,
+            //        Prix = (double)item.Prix,
+            //        Description = new DescriptionData
+            //        {
+            //            CodeDescription = item.Description.IdDescription,
+            //            CodeISBN = item.Description.CodeISBN,
+            //            Detail = item.Description.Detail
+            //        },
+            //        LaCategory = new LivreCategoryDatas(item.FkLivreCategory).ListeCategory.FirstOrDefault()
+            //    });
+            //}
+            #endregion
+            #region version 2
+            var liste2 = context.Livres
+                .Where(c => c.FkLivreCategory == codeCategory)
+                .ToList();
+            foreach (var livre in liste2)
+            {
+                ListeLivre.Add(new LivreData
+                {
+                    CodeISBN = livre.CodeISBN,
+                    NomLivre = livre.NomLivre,
+                    Auteur = livre.Auteur,
+                    Editeur = livre.Editeur,
+                    CoverImage = livre.CoverImage,
+                    Prix = (double)livre.Prix,
+                    Description = new DescriptionData
+                    {
+                        CodeDescription = livre.Description.IdDescription,
+                        CodeISBN = livre.Description.CodeISBN,
+                        Detail = livre.Description.Detail
+                    },
+                    LaCategory = new LivreCategoryData
+                    {
+                        CodeCategory = livre.LivreCategory.IdCategory,
+                        NomCategory = livre.LivreCategory.NomCategory
+                    }
+                });
+
+            }
+            #endregion
+
         }
         #endregion
 
