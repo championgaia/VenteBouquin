73ca37a53fa6b2b8fd1fe441d59a074ac22da4a6
unknown <championgaia@gmail.com>
2019-05-28 17:07:03 +0200

    last commit 28052019

---
+++ DesignPattern/WF01_01_MyFirstWF/html js/JavascriptAvance/JavaScript.js
@@ -0,0 +1,72 @@
+﻿function direBonjour(nom) {
+    var texte = 'Bonjour ' + nom; // Variable locale 
+    var dire = function () { console.log(texte); }
+    return dire;
+}
+var maFonction = direBonjour('Jean'); maFonction(); // logs Bonjour Jean
+
+function multiplicationParDeux() {
+    // Variable locale utilisée pour démontrer la closure 
+    var chiffre = 10;
+    var fonction = function () { console.log(chiffre * multiplicateur); chiffre = chiffre * 2; }
+    var multiplicateur = 10;
+    return fonction;
+}
+var maFonction = multiplicationParDeux();
+maFonction(); // Affiche 100 dans la console
+var maFonction = multiplicationParDeux();
+maFonction(); // Affiche 100 dans la console 
+maFonction(); // Affiche 200 dans la console 
+maFonction = multiplicationParDeux();
+maFonction();
+
+function Client(_nom, _prenom) {
+    this.nom = _nom;
+    this.prenom = _prenom;
+    this.getNomComplet = function () { return this.prenom + ' ' + this.nom; }
+}
+var c = new Client('Foucault', 'Patrick');
+console.log(c.getNomComplet());
+
+function Client(_nom, _prenom) {
+    var age = 50;
+    this.nom = _nom;
+    this.prenom = _prenom;
+    this.getNomComplet = function () { return this.prenom + ' ' + this.nom; }
+    this.getAge = function () { return age++; }
+}
+var c = new Client('Foucault', 'Patrick');
+console.log(c.getAge());
+console.log(c.getAge());
+console.log(c.age);
+
+function Animal(_nom) { this.nom = _nom; }
+Animal.prototype.crier = function () {
+    console.log('Grrrrrrr');
+}
+var a1 = new Animal('Médor');
+a1.crier();
+var a2 = new Animal('Simba');
+a2.crier();
+Animal.prototype.crier = function () {
+    console.log('Miaou');
+}
+a1.crier();
+a2.crier();
+
+
+function Vehicule(_marque, _model) {
+    var vitesse = 0;
+    this.model = _model;
+    this.marque = _marque;
+    this.getVitesse = function () {
+        return vitesse;
+    }
+    Vehicule.prototype.accelerer = function (addition) {
+        vitesse += addition;
+    }
+}
+var ferrary = new Vehicule('Ferrarie', 'GTO');
+console.log('premiere vitesse=' + ferrary.getVitesse());
+ferrary.accelerer(10);
+console.log(' nouvelle vitesse=' +ferrary.getVitesse());
---
+++ DesignPattern/WF01_01_MyFirstWF/html js/jeu taquin/site.js
@@ -0,0 +1,113 @@
+﻿var monTable = [];
+var tableBut = [];
+var buttonVideId = '';
+var buttonCliqueId = '';
+$(function () {
+    //remplir array
+    fillArray(monTable);
+    fillArray(tableBut);
+    //button Start
+    $('.btnStart').click(function () {
+        //shuffle array
+        shuffle(monTable);
+        //remplir le tableau
+        remplirTableau(monTable);
+        //initialiser la valeur du button Undo
+        $('.btnUndo').attr('value','0');
+        if (isGagner(tableBut)) {
+            alert('vous etes gagne');
+        }
+    });
+    //deplacer les cases
+    $('.btnJeu').click(function () {
+        if ($(this).html() === '') {
+            alert('il faut cliquer sur le button start');
+        }
+        else {
+            //prendre la coordinate de deux buttons
+            var coordinateButtonVide = $('#' + buttonVideId).attr('value').split("_");
+            var coordinateButtonClique = $(this).attr('value').split("_");
+            var differentRow = Math.abs(coordinateButtonVide[0] - coordinateButtonClique[0]);
+            var differentColonne = Math.abs(coordinateButtonVide[1] - coordinateButtonClique[1]);
+            //check si valable le button cliqué
+            if ((differentColonne * differentRow) === 0 && (differentRow + differentColonne) === 1) {
+                $('.btnUndo').attr('value', buttonVideId);
+                buttonCliqueId = $(this).attr('id');
+                echangeValue(buttonCliqueId, buttonVideId);
+            }
+            else {
+                alert('choose autre case');
+            }
+            if (isGagner(tableBut)) {
+                alert('vous etes gagne');
+            }
+        }
+    });
+    $('.btnUndo').click(function () {
+        if ($(this).attr('value') == null ) {
+            alert('il faut cliquer sur le button start');
+        }
+        else if ($(this).attr('value') === '0') {
+            alert('vous n\'avez pas encore joue');
+        }
+        else {
+            buttonCliqueId = $(this).attr('value');
+            echangeValue(buttonCliqueId, buttonVideId);
+        }
+    });
+    $('.btnRestart').click(function () {
+        $('.btnVide').removeClass('btnVide');
+        remplirTableau(monTable);
+        
+        $('.btnUndo').attr('value', '0');
+        if (isGagner(tableBut)) {
+            alert('vous etes gagne');
+        }
+    });
+});//fin du document ready
+//remplir le tableau de 1-15 et X
+function fillArray(array) {
+    for (var i = 1; i < 16; i++) {
+        array.push(i);
+    }
+    array.push('X');
+}
+//mélanger le tableau
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+//remplir le tableau 4*4 de jeu
+function remplirTableau(array) {
+    for (var i = 0; i < array.length; i++) {
+        $('#btn' + (i + 1)).html(array[i]);
+        hiddenCase('#btn' + (i + 1));
+    }
+}
+//hide la case vide
+function hiddenCase(chain) {
+    if ($(chain).html() == 'X') {
+        $('.btnVide').removeClass('btnVide');
+        $(chain).addClass('btnVide');
+        buttonVideId = $(chain).attr('id');
+    }
+}
+//echange value de 2 buttons
+function echangeValue(idButtonClique, idButtonVide) {
+    var temp = $('#' + idButtonClique).html();
+    $('#' + idButtonClique).html('X');
+    $('#' + idButtonVide).html(temp);
+    hiddenCase('#' + idButtonClique);
+}
+//check gagne
+function isGagner(array) {
+    var gagne = false;
+    for (var i = 0; i < array.length; i++) {
+        if ($('#btn' + (i + 1)).html() != array[i]) {
+            gagne = false;
+            break;
+        }
+        else
+            gagne = true;
+    }
+    return gagne;
+}
