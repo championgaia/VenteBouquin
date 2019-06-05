892a4eea0554b77a92d998267c8b187cb3509190
Championgaia <championgaia@gamil.com>
2019-06-05 21:27:53 +0200

    finish gridview control

--- DesignPattern/WF01_01_MyFirstWF/ViewWebForm/WF01_05_GridViewControl_DataBinding.aspx.cs
+++ DesignPattern/WF01_01_MyFirstWF/ViewWebForm/WF01_05_GridViewControl_DataBinding.aspx.cs
@@ -4,34 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WF01_01_MyFirstWF.ViewModels;
 
 namespace WF01_01_MyFirstWF.ViewWebForm
 {
-    public partial class WF01_05_GridViewControl_DataBinding : System.Web.UI.Page
+    public partial class WF01_05_GridViewControl_DataBinding : Page
     {
+        private ListLivreViewModel NotreLivres;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                Session["MyBook"] = new ListLivreViewModel();
+            }
+            LivreBinding();
         }
-
-        protected void gvBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        #region LivreBinding
+        private void LivreBinding()
         {
-
+            NotreLivres = new ListLivreViewModel();
+            #region ListView
+            gvBook.DataSource = NotreLivres.ListBookVM;
+            gvBook.DataBind();
+            #endregion
+            //#region textbox
+            //txbTotal.Text = NotreLivres.TotalPrixVM.ToString();
+            //txbTotal.DataBind();
+            //#endregion
         }
-
+        #endregion
         protected void gvBook_RowEditing(object sender, GridViewEditEventArgs e)
         {
-
+            ltError.Text = string.Empty;
+            gvBook.EditIndex = e.NewEditIndex;
+            LivreBinding();
         }
 
         protected void gvBook_RowUpdating(object sender, GridViewUpdateEventArgs e)
         {
+            ltError.Text = string.Empty;
+            GridViewRow gvRow = (GridViewRow)gvBook.Rows[e.RowIndex];
+            HiddenField hdnBookId = (HiddenField)gvRow.FindControl("hdnBookId");
+            TextBox txtNomLivre = (TextBox)gvRow.Cells[1].Controls[0];
+            TextBox txtNomPrix = (TextBox)gvRow.Cells[2].Controls[0];
+            //modify database
 
+            gvBook.EditIndex = -1;
+            LivreBinding();
         }
 
         protected void gvBook_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
         {
+            gvBook.EditIndex = -1;
+            LivreBinding();
+        }
 
+        protected void btnAddRow_Click(object sender, EventArgs e)
+        {
+            //modify database
+
+            gvBook.EditIndex = -1;
+            LivreBinding();
+        }
+        protected void gvBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            ltError.Text = string.Empty;
+            GridViewRow gvRow = (GridViewRow)gvBook.Rows[e.RowIndex];
+            HiddenField hdnBookId = (HiddenField)gvRow.FindControl("hdnBookId");
+            //modify database
+
+            gvBook.EditIndex = -1;
+            LivreBinding();
         }
     }
 }
